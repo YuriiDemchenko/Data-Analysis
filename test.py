b02004acb4f1283e3d027ba65402e998bbb8d48e
Yurii Demchenko <djdomovoy@gmail.com>
2025-01-04 17:37:34 +0200

    First DA tasks

---
+++ test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+cryptoList = ['BTC', 'ETH', 'XRP', 'XLM', 'ANKR', 'ICP', 'PEPE', 'DOGE', 'SHIB', 'ADA']
+cryptoPrices = [45000, 3000, 1.5, 0.5, 0.1, 50, 0.5, 0.3, 0.00001, 2.5]
+cryptoSeries = pd.Series(data = cryptoList)
+print(cryptoSeries)
+
+cryptoPricesSeries = pd.Series(data = cryptoPrices, index = cryptoList)
+print(cryptoPricesSeries)
+
+print('First 3:\n', cryptoPricesSeries.head(3))
+print('First 3 most valuable:\n', cryptoPricesSeries.nlargest(3))
+
+stockListDict = {'AAPL': 150, 'GOOGL': 2800, 'AMZN': 3500, 'TSLA': 700}
+stockSeries = pd.Series(stockListDict)
+print(stockSeries)
+print(stockSeries.shape)
+print(stockSeries.sum())
+print('Last 2 elements in stockSeries:\n', stockSeries.tail(2))
+print(stockSeries.memory_usage())
